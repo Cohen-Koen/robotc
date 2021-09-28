ab41edf1a958f8556860a520fb5f11b70785010e
cheddz-human <spencer.hebel@icloud.com>
2021-09-28 17:45:45 -0500

    YE S

--- tankcontrol.c
+++ tankcontrol.c
@@ -24,41 +24,46 @@ void fowardBackwards(forw,back,time)
 
 task main()
 {
-	if(vexRT[Btn6D] == true) {
+	if(vexRT[Btn6D] == true) {//-----------------------------------------------SETS CLAW TO MOVE \/
 		motor[claw]
 	}
 
 	repeat(forever) { //------------------------------SELF EXPLANITORY
-		//tankControl(Ch3,Ch2,125);
+		//tankControl(Ch3,Ch2,125);--------------------OLD TANK CONTROL
 	motor[leftMotor] = vexRT[Ch3]; //-------------------MOTORS ARE MOTORS
 	motor[rightMotor] = vexRT[Ch2]; //------------------THEY TAKE VALUES 0 TO 127
 
 			// autonomous
 // Idea 2 -------------------------------------------------------------CODE FLOW
 
-	if (vexRT[Btn6D == true]) {
-		setMultipleMotors(127 , motor[leftMotor], motor[rightMotor]);
+	if (vexRT[Btn6D == true]) {//-------------------------------------------------AUTONOMOUS IDEA
+		setMultipleMotors(127 , motor[leftMotor], motor[rightMotor]);//              |
+	//                                                                              \/
 	}
 
 
 
 
-// Idea 1
+// IDEA 1 CHRONOLOGICAL AUTONOMOUS IDEA-----------------------------------  |
+//                                                                          |
+			if (vexRT[Btn8R])//                                             |
+		{//                                                                 |
+			motor[leftMotor] = 127;//                                       |
+			motor[rightMotor] = 127;//                                      |
+			stopAllMotors();          //                                    |
+			wait1Msec(1000);//                                              |
+			backward(1, rotations, 127);//                                  |
+			stopAllMotors();//                                              |
+		}//                                                                 |
+}//                                                                        \/
 
-			if (vexRT[Btn8R])
-		{
 
-			motor[leftMotor] = 127;
-			motor[rightMotor] = 127;
-			stopAllMotors();
-			wait1Msec(1000);
-			backward(1, rotations, 127);
-			stopAllMotors();
-		}
+
+
+
 }
-
-
-
-
-
-}
+//	  \\
+//     (o>
+//  \\_//)
+//   \_/_)
+//    _|_
