62917c2d16a443df9bfdb40eaf93af4a4d6b6ffe
コーエン <deathbydickey@gmail.com>
2021-10-11 16:59:36 -0500

    ay got all the code working

--- tankcontrol.c
+++ tankcontrol.c
@@ -7,63 +7,53 @@ DEVELOPED BY: Cohen Dickey & Spencer Hebel
 */
 
 
-	//Initializing forwards backwards and the time ---------------------------------------------------FUNCTION
-float forw;
-float back;
-float time;
-void fowardBackwards(forw,back,time)
-{
-	setMultipleMotors(127, motor[leftMotor], motor[rightMotor]);
-	wait1Msec(3000);
-	stopAllMotors();
+//	//Initializing forwards backwards and the time ---------------------------------------------------FUNCTION
+//float forw;
+//float back;
+//float time;
+//void fowardBackwards(forw,back,time)
+//{
+//	setMultipleMotors(127, motor[leftMotor], motor[rightMotor]);
+//	wait1Msec(3000);
+//	stopAllMotors();
 
 
-}
+//}
 
 
 
-task main()
-{
-	if(vexRT[Btn6D] == true) {//-----------------------------------------------SETS CLAW TO MOVE \/
-		motor[claw]
-	}
+//task main()
+//{
+//	if(vexRT[Ch6D] == true) {
+//		motor[claw] = 127;
+
+//	}
 
 	repeat(forever) { //------------------------------SELF EXPLANITORY
-		//tankControl(Ch3,Ch2,125);--------------------OLD TANK CONTROL
+		//tankControl(Ch3,Ch2,125);
 	motor[leftMotor] = vexRT[Ch3]; //-------------------MOTORS ARE MOTORS
 	motor[rightMotor] = vexRT[Ch2]; //------------------THEY TAKE VALUES 0 TO 127
 
 			// autonomous
 // Idea 2 -------------------------------------------------------------CODE FLOW
 
-	if (vexRT[Btn6D == true]) {//-------------------------------------------------AUTONOMOUS IDEA
-		setMultipleMotors(127 , motor[leftMotor], motor[rightMotor]);//              |
-	//                                                                              \/
-	}
+	//if (vexRT[Btn6D == true]) {
+	//	setMultipleMotors(127 , motor[leftMotor], motor[rightMotor]);
+	//}
 
 
 
 
-// IDEA 1 CHRONOLOGICAL AUTONOMOUS IDEA-----------------------------------  |
-//                                                                          |
-			if (vexRT[Btn8R])//                                             |
-		{//                                                                 |
-			motor[leftMotor] = 127;//                                       |
-			motor[rightMotor] = 127;//                                      |
-			stopAllMotors();          //                                    |
-			wait1Msec(1000);//                                              |
-			backward(1, rotations, 127);//                                  |
-			stopAllMotors();//                                              |
-		}//                                                                 |
-}//                                                                        \/
+// Idea 1
 
+		//	if (vexRT[Btn8R])
+		//{
 
-
-
-
+		//	motor[leftMotor] = 127;
+		//	motor[rightMotor] = 127;
+		//	stopAllMotors();
+		//	wait1Msec(1000);
+		//	backward(1, rotations, 127);
+		//	stopAllMotors();
+		//}
 }
-//	  \\
-//     (o>
-//  \\_//)
-//   \_/_)
-//    _|_
